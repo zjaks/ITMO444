0740f86b25583077537fc81c870395b8c3967d4a
zjaks <zdenekjaks@gmail.com>
2016-10-14 21:22:46 +0000

    Modified script for formatting and other issues. Need to fix wait command still.

--- create-env.sh
+++ create-env.sh
@@ -1,34 +1,23 @@
 #!/bin/bash
-echo ****************************************
-echo ****************************************
-echo ****************************************
-echo Starting environment creation...
-echo ****************************************
-echo ****************************************
-echo ****************************************
-echo Creating launch configuration...
-echo ****************************************
-echo ****************************************
-aws autoscaling create-launch-configuration --launch-configuration-name launch-config-1 --image-id ami-06b94666 --key-name week3 --instance-type t2.micro --user-data file://installenv.sh
-echo ****************************************
-echo Creating load balancer...
-aws elb create-load-balancer --load-balancer-name balancer-1 --listeners "Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80" --subnets subnet-1fb2917b --security-groups sg-ae6ba2d7 --placement AvailabilityZone=us-west-2b
-echo ****************************************
-echo ****************************************
-echo ****************************************
-echo Creating auto-scaling group...
-aws autoscaling create-auto-scaling-group --auto-scaling-group-name scaling-group-1 --launch-configuration launch-config-1 --availability-zone us-west-2b --load-balancer-names balancer-1 --max-size 5 --min-size 2 --desired-capacity 3
-echo ****************************************
-echo ****************************************
-echo ****************************************
-echo Waiting on instance to be created...
-echo ****************************************
-echo ****************************************
-echo ****************************************
-echo Done!
-echo ****************************************
-echo ****************************************
-echo ****************************************
-echo -e Here are your instance IDs: \n
-aws ec2 describe-instances --filters  "Name=instance-state-name,Values=pending,running,stopped,stopping" --query "Reservations[].Instances[].[InstanceId]" --output text | tr '\n' ' '
 
+echo -e Starting environment creation...'\n'
+
+echo -e Creating launch configuration...'\n'
+
+aws autoscaling create-launch-configuration --launch-configuration-name launch-config-1 --image-id ami-06b94666 --key-name week3 --instance-type t2.micro --user-data file://installenv.sh
+
+echo -e Creating load balancer...'\n'
+
+aws elb create-load-balancer --load-balancer-name balancer-1 --listeners "Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80" --subnets subnet-1fb2917b --security-groups sg-ae6ba2d7
+
+echo -e Creating auto-scaling group...'\n'
+
+aws autoscaling create-auto-scaling-group --auto-scaling-group-name scaling-group-1 --launch-configuration launch-config-1 --availability-zone us-west-2b --load-balancer-names balancer-1 --max-size 5 --min-size 2 --desired-capacity 3
+
+echo -e Waiting on instance to be created...'\n'
+
+echo -e Done!'\n'
+
+echo -e Here are your instance IDs: '\n'
+
+aws ec2 describe-instances --filters  "Name=instance-state-name,Values=pending,running,stopped,stopping" --query "Reservations[].Instances[].[InstanceId]" --output text | tr '\n' ' '
