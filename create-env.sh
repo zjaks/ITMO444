60fa30721c6d4c9fe4696dc2f395500d996cb250
zjaks <zdenekjaks@gmail.com>
2016-10-15 07:20:44 +0000

    Script will now work properly and take in values inputted from the command line.

--- create-env.sh
+++ create-env.sh
@@ -1,25 +1,42 @@
 #!/bin/bash
 
-echo -e Starting environment creation...'\n'
+if [ $# != 5 ]
+then
+	echo -e This requires 5 parameters to run: AMI ID, key-name, security-group, launch-configuration, and count. Please enter these in that order. '\n'
+else
+
+echo "Starting environment creation..."
+
+echo -e "Please enter the AMI ID: $1" 
+
+echo -e "Please enter the key-name: $2" 
+
+echo -e "Please enter the security group number: $3" 
+
+echo -e "Please enter the launch configuration name: $4"  
+
+echo -e "Please enter the count desired:" 
 
 echo -e Creating launch configuration...'\n'
 
-aws autoscaling create-launch-configuration --launch-configuration-name launch-config-1 --image-id ami-06b94666 --key-name week3 --instance-type t2.micro --user-data file://installenv.sh
+aws autoscaling create-launch-configuration --launch-configuration-name $4 --image-id $1 --key-name $2 --instance-type t2.micro --security-groups $3 --user-data file://installenv.sh
 
 echo -e Creating load balancer...'\n'
 
-aws elb create-load-balancer --load-balancer-name balancer-1 --listeners "Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80" --subnets subnet-1fb2917b --security-groups sg-ae6ba2d7
+aws elb create-load-balancer --load-balancer-name balancer-1 --listeners "Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80" --subnets subnet-1fb2917b --security-groups $3
 
 echo -e Creating auto-scaling group...'\n'
 
-aws autoscaling create-auto-scaling-group --auto-scaling-group-name scaling-group-1 --launch-configuration launch-config-1 --availability-zone us-west-2b --load-balancer-names balancer-1 --max-size 5 --min-size 2 --desired-capacity 3
+aws autoscaling create-auto-scaling-group --auto-scaling-group-name scaling-group-1 --launch-configuration $4 --availability-zone us-west-2b --load-balancer-names balancer-1 --max-size 5 --min-size 2 --desired-capacity 3
 
-echo -e Waiting on instances to be created and running...'\n'
+echo -e Waiting on instance to be created...'\n'
 
 aws ec2 wait instance-running --filters --query 'Reservations[].Instances[].InstanceId'
 
-echo -e Complete!'\n'
+echo -e Done!'\n'
 
 echo -e Here are your instance IDs: '\n'
 
 aws ec2 describe-instances --filters  "Name=instance-state-name,Values=pending,running,stopped,stopping" --query "Reservations[].Instances[].[InstanceId]" --output text | tr '\n' ' '
+
+fi
